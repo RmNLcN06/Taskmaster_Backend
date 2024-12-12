ad5c9540fd124653c58c699385f1cc39cfa4dc51
RmNLcN06 <romain.luciano@orange.fr>
2024-12-12 22:22:10 +0100

    Create Project service

---
+++ Taskmaster_Backend/src/main/java/com/rmnlcn/Taskmaster_Backend/services/ProjectService.java
@@ -0,0 +1,36 @@
+package com.rmnlcn.Taskmaster_Backend.services;
+
+import com.rmnlcn.Taskmaster_Backend.entities.Project;
+import com.rmnlcn.Taskmaster_Backend.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProjectService {
+
+    private final ProjectRepository projectRepository;
+
+    @Autowired
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public Optional<Project> getProjectById(Long id) {
+        return projectRepository.findById(id);
+    }
+
+    public Project createProject(Project project) {
+        return projectRepository.save(project);
+    }
+
+    public void deleteProject(Long id) {
+        projectRepository.deleteById(id);
+    }
+}
