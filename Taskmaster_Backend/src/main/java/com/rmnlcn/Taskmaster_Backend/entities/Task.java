ad9444da21c07ed5a40fbf58791670560b19ad1f
RmNLcN06 <romain.luciano@orange.fr>
2024-12-12 21:30:49 +0100

    Create Task entity with annotations.

---
+++ Taskmaster_Backend/src/main/java/com/rmnlcn/Taskmaster_Backend/entities/Task.java
@@ -0,0 +1,25 @@
+package com.rmnlcn.Taskmaster_Backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean completed;
+    private LocalDate dueDate;
+}
